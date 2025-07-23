fc4b2ae9d062f6b99e45f57f06c242c4597327be
riyasisodiya2616 <riyasisodiya2005@gmail.com>
2025-07-23 17:55:31 +0530

    prepare backend for deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,9 @@ import adminRouter from "./routes/admin";
 import userRouter from "./routes/user"
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/v1/admin", adminRouter);
