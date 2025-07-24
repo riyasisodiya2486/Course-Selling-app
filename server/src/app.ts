763b4670a4ce0b1d66f11d7c8e1641cc996d82af
riyasisodiya2616 <riyasisodiya2005@gmail.com>
2025-07-24 22:27:44 +0530

    updations

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,14 @@
 import express from "express";
 import cors from "cors";
 import adminRouter from "./routes/admin";
-import userRouter from "./routes/user"
+import userRouter from "./routes/user";
 
 const app = express();
-app.use(cors({
-    origin: "*"
-}));
+
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/v1/admin", adminRouter);
-app.use('/api/v1/user', userRouter)
+app.use("/api/v1/user", userRouter);
 
-app.listen(4000);
-
+export default app;
